260a4b156d67db994d19fe2272e16700a8f198a2
Nick Pappas <nick@radicand.org>
2020-12-06 19:21:25 -0600

    wip: add more debugging, update bats

--- setup.sh
+++ setup.sh
@@ -232,6 +232,8 @@ function _main
   IMAGE_NAME=${INFO%;*}
   CONTAINER_NAME=${INFO#*;}
   echo "DEBUG: INFO=${INFO}"
+  echo "DEBUG: PRE_IMAGE_NAME=${IMAGE_NAME}"
+  echo "DEBUG: PRE_CONTAINER_NAME=${CONTAINER_NAME}"
 
   if [[ -z ${IMAGE_NAME} ]]
   then
@@ -243,6 +245,8 @@ function _main
       IMAGE_NAME=docker.io/${NAME}
     fi
   fi
+
+  echo "DEBUG: POST_IMAGE_NAME=${IMAGE_NAME}"
 
   if tty -s
   then
